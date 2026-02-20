c6d08942dd6b9f2f531d314842e85ee96b2d97d2
VanessaKang <160397233+manglibopaul@users.noreply.github.com>
2026-02-20 00:13:29 +0000

    Add byTheme method to ProductController

--- MANGLIBO_Midterm_Exam_Application/app/Http/Controllers/ProductController.php
+++ MANGLIBO_Midterm_Exam_Application/app/Http/Controllers/ProductController.php
@@ -6,17 +6,28 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    private $products = [
+        ['title' => 'Inception', 'genre' => 'Science Fiction'],
+        ['title' => 'The Godfather', 'genre' => 'Crime'],
+        ['title' => 'Interstellar', 'genre' => 'Adventure'],
+        ['title' => 'Parasite', 'genre' => 'Thriller'],
+        ['title' => 'Spirited Away', 'genre' => 'Animation'],
+        ['title' => 'The Dark Knight', 'genre' => 'Action'],
+    ];
+
+    // Show all products
     public function index()
     {
-        $products = [
-            ['title' => 'Inception', 'genre' => 'Science Fiction'],
-            ['title' => 'The Godfather', 'genre' => 'Crime'],
-            ['title' => 'Interstellar', 'genre' => 'Adventure'],
-            ['title' => 'Parasite', 'genre' => 'Thriller'],
-            ['title' => 'Spirited Away', 'genre' => 'Animation'],
-            ["title" => "The Dark Knight", "genre" => "Action"],
-        ];
-        return $products;
+        return view('products', ['products' => $this->products]);
+    }
+
+    // Show products by selected theme (genre)
+    public function byTheme($theme)
+    {
+        $filtered = array_filter($this->products, function($product) use ($theme) {
+            return strtolower($product['genre']) === strtolower($theme);
+        });
+        return view('products', ['products' => $filtered, 'theme' => $theme]);
     }
 }
 
